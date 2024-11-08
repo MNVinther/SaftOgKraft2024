ff8e37327ac9cfa69bf409ea064fbcdfa6c8c0f5
SimonLund192 <10503079@ucn.dk>
2024-11-08 10:45:49 +0100

    WebApiClient

---
+++ WebApiClient/IRestClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiClient.DTOs;
+
+namespace WebApiClient
+{
+    public interface IRestClient
+    {
+        IEnumerable<Product> GetTenLatestProducts();
+        Product GetProductFromPartOfName(string partOfName);
+        int AddProduct(Product product);
+
+    }
+}
