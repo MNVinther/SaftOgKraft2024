8dfe839f680777afd44a0be29405233afa20f679
SimonLund192 <10503079@ucn.dk>
2024-11-12 09:19:35 +0100

    DAL umiddelbart klar til at blive forbundet

---
+++ DAL/DAO/BaseDAO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DAO
+{
+    public abstract class BaseDAO
+    {
+        private string _connectionString;
+
+        public BaseDAO(string connectionString) => _connectionString = connectionString;
+
+        protected IDbConnection CreateConnection();
+        {
+            return new SQLConnection(_connectionString);
+        }
+
+    }
+}
---
+++ DAL/DAO/IProductDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DAO
+{
+    public interface IProductDAO
+    {
+        IEnumerable<Product> GetAll();
+        Product Get(int id);
+        int Insert(Product product);
+        IEnumerable<Product> GetTenLatestProducts();
+    }
+}
